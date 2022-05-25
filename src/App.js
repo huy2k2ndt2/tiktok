884da568882f9dfc3a219b9b808efa501b044283
huy2k2ndt2 <huy2k2ndt2@gmail.com>
2022-05-25 20:41:34 +0700

    config axios

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/Layouts";
+import { Fragment, useEffect } from "react";
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route) => {
+                        const Layout = route.layout ? route.layout : route.layout === null ? Fragment : DefaultLayout;
+
+                        const Page = route.component;
+
+                        return (
+                            <Route
+                                key={route.key}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from "uuid";
+
+// Layouts
+import { HeaderOnly } from "@/components/Layouts";
+
+// Pages
+import Home from "@/pages/Home";
+import Following from "@/pages/Following";
+import Profile from "@/pages/Profile";
+import Upload from "@/pages/Upload";
+import Login from "@/pages/Login";
+import Search from "@/pages/Search";
+
+// Public routes
+const publicRoutes = [
+    { path: "/", component: Home, key: uuidv4() },
+    { path: "/login", component: Login, key: uuidv4() },
+    { path: "/following", component: Following, key: uuidv4() },
+    { path: "/@:nicknam", component: Profile, key: uuidv4() },
+    { path: "/upload", component: Upload, layout: HeaderOnly, key: uuidv4() },
+    { path: "/search", component: Search, layout: null, key: uuidv4() },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
